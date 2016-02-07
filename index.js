149caf9f10daff930de0d87732eaee6d37a597eb
ikr7 <kunpei.ikuta+github@gmail.com>
2016-02-07 21:30:26 +0900

    added filename extension to argument of require()

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.Machine = require('./lib/machine');
-exports.Blocks = require('./lib/blocks');
+exports.Machine = require('./lib/machine.js');
+exports.Blocks = require('./lib/blocks.js');
